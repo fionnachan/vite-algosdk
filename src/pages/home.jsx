e65b2b4aea21a86cdc6ff001ee90ad768daa357b
Glitch (glitch-hello-react) <none>
2021-12-29 06:30:31 +0000

    📻🐕 Checkpoint ./src/pages/home.jsx:412758/237

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import algosdk from 'algosdk';
 import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
@@ -34,6 +35,12 @@ export default function Home() {
      - We can attach this to events on elements in the page and apply the resulting style
   */
   const [style, trigger] = useWiggle({ x: 5, y: 5, scale: 1 });
+  const indexer = new algosdk.Indexer("","https://algoindexer.algoexplorerapi.io/v2", "");
+  indexer.lookupAssetById(384303832).do()
+    .then(response => {
+
+      console.log("response", response)
+    });
 
   // When the user clicks we change the header language
   const handleChangeHello = () => {
