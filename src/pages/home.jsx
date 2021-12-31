0a819418f455df91453a7dfe62b590a03ea23c53
Glitch (glitch-hello-react) <none>
2021-12-31 06:30:00 +0000

    🎐🌁 Checkpoint ./src/pages/home.jsx:412758/2569

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,5 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import algosdk from 'algosdk';
-import { animated } from "react-spring";
-import { useWiggle } from "../hooks/wiggle";
-import { Link } from "wouter";
 
 /**
 * The Home function defines the content that makes up the main content of the Home page
@@ -10,34 +7,32 @@ import { Link } from "wouter";
 * This component is attached to the /about path in router.jsx
 * The function in app.jsx defines the page wrapper that this appears in along with the footer
 */
-
 export default function Home() {
-  
-  /* The wiggle function defined in /hooks/wiggle.jsx returns the style effect and trigger function
-     - We can attach this to events on elements in the page and apply the resulting style
-  */
-  const [style, trigger] = useWiggle({ x: 5, y: 5, scale: 1 });
-  const indexer = new algosdk.Indexer("","https://algoindexer.algoexplorerapi.io/", "");
-  indexer.lookupAssetByID(384303832).do()
-    .then(response => {
-
-      console.log("response", response)
-    });
+  const runIndexerMethods = async () => {
+    const token  = "";
+    const server = "https://algoindexer.algoexplorerapi.io/";
+    const port   = "";
+    const indexer = new algosdk.Indexer(token, server, port);
+    const assetId = 384303832;
+    const address = "XBYLS2E6YI6XXL5BWCAMOA4GTWHXWENZMX5UHXMRNWWUQ7BXCY5WC5TEPA";
+    const accountInfo = await indexer.lookupAccountByID(address).do();
+    const assetInfo = await indexer.lookupAssetByID(assetId).do();
+    console.log("lookupAssetByID response:", response)
+  }
+  useEffect(() => {
+    runIndexerMethods();
+  }, [runIndexerMethods]);
   return (
     <>
       <h1 className="title">Hello!</h1>
       <div className="navigation">
-        <button className="btn--click-me">
-          Psst, click me
-        </button>
+        Open your console to see the results!
       </div>
       <div className="instructions">
         <h2>Using this project</h2>
         <p>
-          This is a <strong>Vite + Algorand JS SDK</strong> project. You can use
-          it to build your own app. See more info in the{" "}
-          <Link href="/about">About</Link> page, and check out README.md in the
-          editor for additional detail plus next steps you can take!
+          This is a <strong>Vite + Algorand JS SDK</strong> project. It comes with the polyfill for Buffer to allow Algorand JS SDK to work. You can use
+          it to build your own app.
         </p>
       </div>
     </>
