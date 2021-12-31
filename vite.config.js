71d05bff850ed220f470571e36c187fcb5e8c137
Glitch (glitch-hello-react) <none>
2021-12-31 06:18:03 +0000

    🏆⛑ Checkpoint ./index.html:412758/1490 ./package.json:412758/1907 ./vite.config.js:412758/31 ./src/pages/home.jsx:412758/1581

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
       process: "process/browser",
       stream: "stream-browserify",
       zlib: "browserify-zlib",
-      util: 'util'
+      util: 'util',
+      path: "path-browserify"
     }
   }
 });
